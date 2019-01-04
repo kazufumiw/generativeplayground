61b65e693e5bd4fc449f2d47a7d78f0a0b866c92
渡邉一史 <watanabekazufumi@watanabekazufuminoMacBook-Air.local>
2019-01-04 19:16:35 +0900

    add a work

---
+++ colorparette/sketch.js
@@ -0,0 +1,26 @@
+var max_res = 50;
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0,0,0);
+	//rectMode(CENTER);
+	colorMode(HSB);
+	noStroke();
+}
+
+function draw() {
+  var res_x = max_res*mouseX/windowWidth + 1;
+  var res_y = max_res*mouseY/windowHeight + 1;
+  var dx = windowWidth/res_x;
+  var dy = windowHeight/res_y;
+  for(var i = 0; i < res_y; i += 1){
+    for(var j = 0; j < res_x; j += 1){
+      fill(360*(j + 1)/res_x, 100*(res_y - i - 1)/res_y, 100)
+      rect(dx*j, dy*i, dx, dy);
+    }
+  }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    //background(0,0,0);
+}
