0255219c6f7eda6ab4cbdae27d3155a8d245f408
kazufumiw <Kazufumi.Watanabe1@ibm.com>
2019-01-10 00:04:41 +0900

    add posts

---
+++ bayerpattern_fornonretina/sketch_bayer.js
@@ -0,0 +1,60 @@
+var capture;
+var nx = 96;
+var ny = 64;
+var tile;
+var index;
+var colour;
+var greyscale;
+var trans;
+var mode = 0;
+var count = 0;
+var framecount = 0;
+
+function preload(){
+	capture = createCapture(VIDEO);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(RGB);
+  clear();
+	noStroke();
+  frameRate(24);
+  capture.size(nx, ny);
+  capture.hide();
+  background(0);
+  capture.loadPixels();
+
+  if(windowWidth/nx < windowHeight/ny){
+    tile = windowWidth/nx;
+	}
+	else{
+		tile = windowHeight/ny;
+	}
+  trans = (windowWidth-nx*tile)*0.5;
+  console.log(nx, ny, tile, trans);
+}
+
+function draw() {
+  background(0);
+  framecount = framecount + 0.25;
+  for(var i = 0; i < nx; i++){
+    for(var j = 0; j < ny; j++){
+      colorMode(RGB);
+      colour = color(capture.get(i, j));
+      greyscale = round(red(colour)*0.222+green(colour)*0.707+blue(colour)*0.071);
+      rectMode(CORNER);
+			fill(0, green(colour), 0);
+			rect(tile*i+trans, tile*j, ((i+j)%2)*tile, ((i+j)%2)*tile);
+			fill(red(colour), 0, 0);
+      rect(tile*i+trans, tile*j, (i%2)*tile, (j%2)*tile);
+			fill(0, 0, blue(colour));
+			rect(tile*i+trans, tile*j, ((i+1)%2)*tile, ((j+1)%2)*tile);
+
+    }
+  }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
